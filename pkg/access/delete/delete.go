41b56137cad1c8a7d900505d1e327ef4277c562c
onyiny-ang <ltulloch@redhat.com>
2018-02-28 09:18:31 -0500

    modified delete to actually handle authinfo

--- pkg/access/delete/delete.go
+++ pkg/access/delete/delete.go
@@ -42,7 +42,7 @@ func NewCmdDelete(cmdOut io.Writer) *cobra.Command {
 			if err != nil {
 				glog.Fatalf("error: %v", err)
 			}
-			err = opts.validateFlags(pathOptions, hostConfig)
+			err = opts.validateFlags(cmdOut, pathOptions, hostConfig)
 			if err != nil {
 				glog.Fatalf("error: %v", err)
 			}
@@ -58,13 +58,14 @@ func NewCmdDelete(cmdOut io.Writer) *cobra.Command {
 
 }
 
-func (o *deleteOptions) validateFlags(pathOptions *clientcmd.PathOptions, hostConfig *rest.Config) error {
+func (o *deleteOptions) validateFlags(cmdOut io.Writer, pathOptions *clientcmd.PathOptions, hostConfig *rest.Config) error {
 	//ensure Cluster Name exists as context
 	config, err := pathOptions.GetStartingConfig()
 	if err != nil {
 		return err
 	}
 	if _, exists := config.Contexts[o.ClusterName]; !exists {
+		fmt.Fprintf(cmdOut, "Could not find any context, %s in your kubeconfig.", o.ClusterName)
 		glog.V(4).Info("error: context %v not found", o.ClusterName)
 		return err
 	}
@@ -74,8 +75,8 @@ func (o *deleteOptions) validateFlags(pathOptions *clientcmd.PathOptions, hostCo
 	}
 	//ensure cluster exists in cluster registry
 	if err := clientset.ClusterregistryV1alpha1().RESTClient().Get().Resource("clusters").Name(o.ClusterName).Do().Error(); err != nil {
+		fmt.Fprintf(cmdOut, "Could not find specified cluster, %s in cluster registry.", o.ClusterName)
 		glog.V(4).Info("error: cluster %v not found", o.ClusterName)
-		fmt.Println(err)
 		return err
 	}
 	return nil
@@ -83,7 +84,6 @@ func (o *deleteOptions) validateFlags(pathOptions *clientcmd.PathOptions, hostCo
 
 func deleteRun(cmdOut io.Writer, opts *deleteOptions, hostConfig *rest.Config, pathOptions *clientcmd.PathOptions, deleteCmd *cobra.Command, args []string) error {
 
-	errCount := 0
 	fmt.Fprintf(cmdOut, "Delete kubeconfig entry %s...", opts.ClusterName)
 	glog.V(4).Infof("Delete kubeconfig entry %s", opts.ClusterName)
 	pathOptions.LoadingRules.ExplicitPath = opts.KubeLocation
@@ -97,33 +97,14 @@ func deleteRun(cmdOut io.Writer, opts *deleteOptions, hostConfig *rest.Config, p
 		kubeconfigFile = pathOptions.GetExplicitFile()
 	}
 
-	_, ok := kubeconfig.Contexts[opts.ClusterName]
-	if !ok {
-		glog.V(4).Infof("cannot delete context %s, not in %s", opts.ClusterName, kubeconfigFile)
-		errCount++
-	} else {
-		delete(kubeconfig.Contexts, opts.ClusterName)
-	}
-	_, ok = kubeconfig.Clusters[opts.ClusterName]
-	if !ok {
-		glog.V(4).Infof("cannot delete cluster %s, not in %s", opts.ClusterName, kubeconfigFile)
-		errCount++
-	} else {
-		delete(kubeconfig.Clusters, opts.ClusterName)
-	}
+	kubeContext, _ := kubeconfig.Contexts[opts.ClusterName]
 
-	_, ok = kubeconfig.AuthInfos[opts.ClusterName]
-	if !ok {
-		glog.V(4).Infof("cannot delete authinfo %s, not in %s", opts.ClusterName, kubeconfigFile)
-		errCount++
-	} else {
-		delete(kubeconfig.AuthInfos, opts.ClusterName)
-	}
+	delete(kubeconfig.Contexts, opts.ClusterName)
 
-	if errCount == 3 {
-		fmt.Fprintf(cmdOut, "Could not find any context, cluster, or authinfo information for %s in your kubeconfig.", opts.ClusterName)
-		glog.Fatalf("Could not find any context, cluster or authinfo for %s in kubeconfig", opts.ClusterName)
-	}
+	delete(kubeconfig.Clusters, opts.ClusterName)
+
+	delete(kubeconfig.AuthInfos, kubeContext.AuthInfo)
+
 	// Write the updated kubeconfig.
 	if err := clientcmd.ModifyConfig(pathOptions, *kubeconfig, true); err != nil {
 		glog.Fatalf("Unable to update kubeconfig, %s", err)
